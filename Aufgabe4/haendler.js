c1a1fbd82c5e1ec5d1708aa0d5df38271ece4320
Sabine <Sabine@Sabine-NB.mshome.net>
2018-12-13 14:34:53 +0100

    Haendler - Datei hinzugefügt

---
+++ Aufgabe4/haendler.js
@@ -0,0 +1,18 @@
+/*
+Aufgabe: Aufgabe 4
+Name: Michelle Zei�ner
+Matrikel: 254665
+Datum: 26.10.18
+
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
+Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var Aufgabe4;
+(function (Aufgabe4) {
+    Aufgabe4.data = {
+        "Baeume": [{ text: "Waehle eine Baumart aus", preis: 20, name: "Kiefer" }, { text: "Waehle eine Baumart aus", preis: 30, name: "Tanne" }, { text: "Waehle eine Baumart aus", preis: 45, name: "Fichte" }],
+        "Kugeln": [{ text: "Waehle Kugeln aus", preis: 5, name: "Rot" }, { text: "Waehle Kugeln aus", preis: 30, name: "Gruen" }, { text: "Waehle Kugeln aus", preis: 30, name: "Blau" }],
+        "Lametta": [{ text: "Waehle Lametta aus", preis: 10, name: "Lametta" }, { text: "Waehle Lametta  aus", preis: 30, name: "Lametta" }],
+    };
+})(Aufgabe4 || (Aufgabe4 = {}));
+//# sourceMappingURL=haendler.js.map
